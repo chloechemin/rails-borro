f4f20d5ea0fd881c0d1429dba92b35a0b04325bf
Francisca Montecinos <franmontecinosoportus@gmail.com>
2022-07-09 15:08:48 +1000

    saving changes to new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
 end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:category, :title, :location, :description, :duration, :owner_id)
+  end
