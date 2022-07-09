dba3cd72d8f486093d3e3e5eab1a4fbb9956e38b
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-07-09 17:23:55 +1000

    Implemented show, changed views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       redirect_to post_path(@post)
     else
