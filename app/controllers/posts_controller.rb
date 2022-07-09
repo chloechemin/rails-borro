ea5650924f4efde8abec44c0f0a1057500b1b52c
Chloe Chemin <chloechemin4@gmail.com>
2022-07-09 14:58:46 +0930

    done the edit actions in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params[:post])
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:category, :title, :description, :location, :duration, :owner_id)
+  end
+
 end
