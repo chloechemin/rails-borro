3625d7efa03f10ccdda240570bfb27dd1da7d314
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-07-12 20:14:35 +1000

    Implemented bookings#show and #delete, updated form entries for posts and added link to helpers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,17 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time, :status, :user_id, :post_id, :booking_id)
+  end
 end
