0a9a46286af69584ee213ef060da15188ba57df4
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-07-14 21:17:14 +1000

    Added location autocomplete for form partial in Posts View

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to post_path
+    redirect_to posts_path
   end
 
   private
