deb70133126c39edbbeaa653c8140945075ea690
ChiChi Luan <qiqi.luan@gmail.com>
2022-07-16 17:01:49 +1000

    updating toggle button

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,9 +10,17 @@ class SearchController < ApplicationController
       AND duration >= :duration "
 
       @posts = Post.where(sql_query, category: "%#{params[:category]}%", query: "%#{params[:description]}%", duration: "#{params[:duration]}".to_f)
+      # raise
+      if @posts.count == 0
+         redirect_to search_no_result_path()
+      end
+
     else
       @posts = Post.all
     end
 
+  end
+
+  def no_result
   end
 end
