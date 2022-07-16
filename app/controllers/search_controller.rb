db99caeb7330faad2237ef62002bc8deae121821
ChiChi Luan <qiqi.luan@gmail.com>
2022-07-16 14:34:28 +1000

    made search function work

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,18 @@
 class SearchController < ApplicationController
   def result
-    @posts = Post.all
+
+    if params[:category].present? || params[:description].present? || params[:duration].present?
+
+      sql_query = " \
+      category LIKE :category \
+      AND (title ILIKE :query \
+      OR description ILIKE :query) \
+      AND duration >= :duration "
+
+      @posts = Post.where(sql_query, category: "%#{params[:category]}%", query: "%#{params[:description]}%", duration: "#{params[:duration]}".to_f)
+    else
+      @posts = Post.all
+    end
+
   end
 end
