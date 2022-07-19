c0d7fb7684c3d038eda3aaeca21132493fb88557
Chloe Chemin <chloechemin4@gmail.com>
2022-07-20 00:22:47 +0930

    Review + quick formating

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,32 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @post = Post.find(params[:post_id])
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    @booking.user = current_user
+    if @review.save
+      redirect_to post_path(@booking.post)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to list_path(@review.list)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+end
