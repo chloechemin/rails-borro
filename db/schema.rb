c1a62c592e6cccbf817972f033991ce601b4b6b8
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-07-19 19:03:48 +1000

    Removed longitude and latitude Helps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_001217) do
+ActiveRecord::Schema.define(version: 2022_07_19_090153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +54,6 @@ ActiveRecord::Schema.define(version: 2022_07_19_001217) do
     t.bigint "help_request_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.index ["help_request_id"], name: "index_helps_on_help_request_id"
     t.index ["user_id"], name: "index_helps_on_user_id"
   end
