ee6c2a749633a84f2424c948009965a9a0d2c1f9
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-07-30 12:14:16 +1000

    Added Noticed model and relationship for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,19 +11,21 @@ class User < ApplicationRecord
   has_many :chatrooms, through: :participants
   has_one_attached :avatar
   after_commit :add_default_avatar, on: %i[create update]
+  has_many :notifications, as: :recipient
 
   def avatar_thumbnail
     if avatar.attached?
       avatar.variant(resize: "150x150!").processed
-   else
-    "/default_profile.jpeg"
-   end
+    else
+      "/default_profile.jpeg"
+    end
   end
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   private
+
   def add_default_avatar
     unless avatar.attached?
       avatar.attach(
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_22_085238) do
+ActiveRecord::Schema.define(version: 2022_07_30_021202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,18 @@ ActiveRecord::Schema.define(version: 2022_07_22_085238) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.bigint "recipient_id", null: false
+    t.string "type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
+  end
+
   create_table "participants", force: :cascade do |t|
     t.bigint "chatroom_id", null: false
     t.bigint "user_id", null: false
@@ -126,7 +138,6 @@ ActiveRecord::Schema.define(version: 2022_07_22_085238) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
@@ -162,5 +173,4 @@ ActiveRecord::Schema.define(version: 2022_07_22_085238) do
   add_foreign_key "participants", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "reviews", "bookings"
-  add_foreign_key "reviews", "users"
 end
