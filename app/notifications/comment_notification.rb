5ef5793a30f666d3d6e697fcbdbb9788190f8a5e
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-07-30 12:48:44 +1000

    Modified comment_notification.rb

--- app/notifications/comment_notification.rb
+++ app/notifications/comment_notification.rb
@@ -6,22 +6,23 @@
 class CommentNotification < Noticed::Base
   # Add your delivery methods
   #
-  # deliver_by :database
+  deliver_by :database
+  deliver_by :action_cable
   # deliver_by :email, mailer: "UserMailer"
   # deliver_by :slack
   # deliver_by :custom, class: "MyDeliveryMethod"
 
   # Add required params
   #
-  # param :post
+  param :post
 
   # Define helper methods to make rendering easier.
   #
-  # def message
-  #   t(".message")
-  # end
-  #
-  # def url
-  #   post_path(params[:post])
-  # end
+  def message
+    t(".message")
+  end
+
+  def url
+    post_path(params[:post])
+  end
 end
