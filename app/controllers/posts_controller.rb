07f0a4eae0169e0449006a9f98c635ee07099e9c
Anuth Hatharasinghe <anuth.hatharasinghe@gmail.com>
2022-08-05 08:28:49 +1000

    Fixed redirect after delete path to the home page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -50,7 +50,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to pages_home_path
   end
 
   private
